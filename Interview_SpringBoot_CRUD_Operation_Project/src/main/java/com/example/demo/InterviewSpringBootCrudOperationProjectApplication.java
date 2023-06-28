c7372b7c906c6f9635cfedf33950d48c498c6189
Pankaj Kumar <Pankaj Kumar@Universe>
2023-06-28 23:00:29 +0530

    1st commit

---
+++ Interview_SpringBoot_CRUD_Operation_Project/src/main/java/com/example/demo/InterviewSpringBootCrudOperationProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewSpringBootCrudOperationProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewSpringBootCrudOperationProjectApplication.class, args);
+	}
+
+}
